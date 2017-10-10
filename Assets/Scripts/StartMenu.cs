9b57249c83de22175614591a172543b9e1e6fa6c
Hassan Alghanim <ha@hassans-mbp.gateway.pace.com>
2017-10-09 21:39:46 -0500

    Added start Menu. Its ugly for now\!

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+    public void Play() {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
